20c3dea20d452e78c39f15ecf913e4c9a2581aaa
Krishan-R <krishan.raithatha274@gmail.com>
2022-07-09 12:09:14 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+
+    file = open('parse.html', 'r')
+    soup = BeautifulSoup(file.read(), features="html.parser")
+
+    streams = {"NiceWigg": 'nicewigg'}
+    ignoreList = ['playapex', 'bbl spanish', 'bbl portuguese',
+                  'nicewigg b stream', 'rage japanese',
+                  'maincast', 'fps thai', '4gamers mandarin ']
+
+    buttons = soup.find_all("button", attrs={'data-target': True})
+    for button in buttons:
+        children = button.findChildren("img", attrs={'alt': True})
+        url = button['data-target']
+        streamName = children[0]['alt'][3::]
+        if streamName.lower() not in ignoreList and url != 'playapex':
+            streams[streamName] = url
+
+    print('| Stream | URL |')
+    print('|-----|-----|')
+    for name in streams:
+        url = streams[name]
+        print(f'| {name} | https://www.twitch.tv/{url} |')
