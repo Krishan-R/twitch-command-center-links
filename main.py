b9c2d62f73610c9d280521e7862cd94274f14ccd
Krishan-R <krishan.raithatha274@gmail.com>
2022-11-06 19:57:57 +0000

    uses tooltip for stream name rather than icon (which may not exist)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
+import re
 
 if __name__ == '__main__':
 
-    file = open('parse.html', 'r')
-    soup = BeautifulSoup(file.read(), features="html.parser")
+    with open("parse.html", 'r') as file:
+        data = file.read()
+    soup = BeautifulSoup(data, features="html.parser")
 
     streams = {"NiceWigg": 'nicewigg'}
     ignoreList = ['playapex', 'bbl spanish', 'bbl portuguese',
@@ -12,9 +14,16 @@ if __name__ == '__main__':
 
     buttons = soup.find_all("button", attrs={'data-target': True})
     for button in buttons:
-        children = button.findChildren("img", attrs={'alt': True})
         url = button['data-target']
-        streamName = children[0]['alt'][3::]
+
+        parent = button.parent
+        name = parent.findChildren('div', recursive=False)[0]
+        streamName = name.getText()
+
+        streamName = re.sub("[\(\[].*?[\)\]]", "", streamName)
+        streamName = streamName.replace("\n", "").strip()
+        streamName = streamName[3::]
+
         if streamName.lower() not in ignoreList and url != 'playapex':
             streams[streamName] = url
 
@@ -24,6 +33,6 @@ if __name__ == '__main__':
         url = streams[name]
         # print(f'| {name} | https://www.twitch.tv/{url} |')
 
-        print(f'streamlink https://www.twitch.tv/{url} best --output "/mnt/pool/aDownloads/vods/ALGS Championships/tosort/ALGS Championships Raleigh - {name} Finals - ' + '{time:%Y-%m-%d}.mkv" &>/dev/null &')
+        print(f'streamlink https://www.twitch.tv/{url} best --output "/mnt/pool/tempdownloads/vods/ALGS PL2 Week 1/tosort/ALGS PL2 Week 1 - {name} - ' + '{time:%Y-%m-%d}.mkv" &>/dev/null &')
 
     print(len(streams))
